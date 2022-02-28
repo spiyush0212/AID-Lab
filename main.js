29db5b99518c18674ebd09d45cac4b1d097f78b9
Piyush Sharma <spiyush.0212@gmail.com>
2022-02-28 11:37:06 +0530

    Basic landing page, about us page and projects page

---
+++ main.js
@@ -0,0 +1,9 @@
+var slider = tns({
+    "container": '.my-slider',
+    "items": 3,
+    "slideBy": "page",
+    "mouseDrag": true,
+    "swipeAngle": false,
+    "speed": 400,
+    "navPosition": "bottom"
+});
