280422d1e348c42bbdfbd51a01c136df29ebfaba
Piyush Sharma <spiyush.0212@gmail.com>
2022-03-22 17:01:51 +0530

    Fix UI mobile responsive

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 // // Carosuel Buttons
 var slider = tns({
     "container": '.my-slider',
-    "items": 3,
+    "items": 2,
+    "responsive": {
+        750: {
+            items: 3
+        }
+    },
     "slideBy": "page",
     "mouseDrag": true,
     "swipeAngle": false,
     "speed": 400,
+    "autoplay": true,
+    "autoplayHoverPause": true,
+    "autoplayButtonOutput": false,
     "navPosition": "bottom"
 });
