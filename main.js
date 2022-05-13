295aacd45ea52f467e38fcc134f274c24517d226
Piyush Sharma <spiyush.0212@gmail.com>
2022-05-13 16:17:38 +0530

    Add 2 new projects

--- main.js
+++ main.js
@@ -27,6 +27,51 @@ window.addEventListener("scroll", function (e) {
 
 // Projects
 
+var all = [
+    {
+        idx: '1',
+        title: 'Story Box',
+        summary: `Independent Multi-modal\nInteractive Storytelling for\nChildren with Visual Impairment`,
+        img: 'imgs/projects/storybox.jpg'
+    },
+    {
+        idx: '2',
+        title: 'Accessible Learning',
+        summary: `Exploring the Design of XR\nEnabled Accessible Learning\nExperiences`,
+        img: 'imgs/projects/xr.jpg'
+    },
+    {
+        idx: '3',
+        title: 'Accessible App',
+        summary: `Accessible Bus Information\nsystem and app for users with\nsensory impairments`,
+        img: 'imgs/projects/bus.jpg'
+    },
+    {
+        idx: '4',
+        title: 'Safety App',
+        summary: `Teaching life skills to blind\nstudents using spatial sound.It\ncaters to the problem through a\nrole-playing game.`,
+        img: 'imgs/projects/safetyapp.png'
+    },
+    {
+        idx: '5',
+        title: 'Healthier Workstation',
+        summary: `A healthier & space efficient\nworkstation that caters to one’s\nphysical & mental health.`,
+        img: 'imgs/projects/workstation.jpg'
+    },
+    {
+        idx: '6',
+        title: 'E-Vision',
+        summary: `E-Vision is a low-cost multiline\nbraille display that enables\naccess to quality and affordable\neducation for the visually challenged.`,
+        img: 'imgs/projects/braille.jpg'
+    },
+    {
+        idx: '7',
+        title: 'Chartr',
+        summary: `It is a GUI design that improves\n the bus ticketing system for\nnon native language speakers`,
+        img: 'imgs/projects/charts.png'
+    }
+]
+
 var accEdu = [
     {
         idx: '1',
@@ -52,8 +97,14 @@ var accWeb = [
     {
         idx: '4',
         title: 'Safety App',
-        summary: `Teaching life skills to blind\n\students using spatial sound.It\n\caters to the problem through a\n\role-playing game.`,
+        summary: `Teaching life skills to blind\n\students using spatial sound.It\n\caters to the problem through a\nrole-playing game.`,
         img: 'imgs/projects/safetyapp.png'
+    },
+    {
+        idx: '7',
+        title: 'Chartr',
+        summary: `It is a GUI design that improves\n the bus ticketing system for\nnon native language speakers`,
+        img: 'imgs/projects/charts.png'
     }
 ]
 
@@ -63,18 +114,25 @@ var accInfo = [
         title: 'Healthier Workstation',
         summary: `A healthier & space efficient\n\workstation that caters to one’s\n\physical & mental health.`,
         img: 'imgs/projects/workstation.jpg'
+    },
+    {
+        idx: '6',
+        title: 'E-Vision',
+        summary: `E-Vision is a low-cost multiline\nbraille display that enables\naccess to quality and affordable\neducation for the visually challenged.`,
+        img: 'imgs/projects/braille.jpg'
     }
 ]
 
 var contain = document.querySelector('.my-slider');
 
+var allBtn = document.querySelector('#allProjects');
 var accEduBtn = document.querySelector('.accEdu');
 var accWebBtn = document.querySelector('.accWeb');
 var accInfoBtn = document.querySelector('.accInfo');
 
 function fill(project) {
     var projectDiv = document.createElement('div');
-    projectDiv.classList.add("col-6", "col-md-4", "text-center", "d-flex", "justify-content-center", "align-items-center");
+    projectDiv.classList.add("col-6", "my-2", "col-md-4", "text-center", "d-flex", "justify-content-center", "align-items-center");
     projectDiv.setAttribute("data-bs-toggle", "modal");
     projectDiv.setAttribute("data-bs-target", `#exampleModal${project.idx}`);
 
@@ -111,8 +169,15 @@ function fill(project) {
 }
 
 window.onload = function () {
-    document.querySelector('.accEdu').click();
+    document.querySelector('.accWeb').click();
 }
+
+allBtn.addEventListener('click', () => {
+    contain.innerHTML = ``;
+    all.forEach(project => {
+        fill(project);
+    });
+});
 
 accEduBtn.addEventListener('click', () => {
     contain.innerHTML = ``;
